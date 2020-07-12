49a4a9c6187bbede0df8ccf8a85c7329107befe6
Rishabh Jain <rishabh@codeation.io>
2020-07-12 23:15:36 +0530

    feat: env contract

---
+++ contracts/env.ts
@@ -0,0 +1,17 @@
+declare module '@ioc:Adonis/Core/Env' {
+  type EnvVariables = 'PORT' | 'HOST' | 'NODE_ENV' | 'APP_KEY' |
+  'SPOTIFY_CLIENT_ID' | 'SPOTIFY_CLIENT_SECRET' | 'DB_CONNECTION' |
+  'DB_HOST' | 'DB_USER' | 'DB_PASSWORD' | 'DB_NAME'
+
+  export interface EnvContract {
+    process(envString: string, overwrite?: boolean): void;
+    parse(
+      envString: EnvVariables
+    ): {
+      [key: string]: string;
+    };
+    get(key: EnvVariables, defaultValue?: any): string | boolean | null | undefined;
+    getOrFail(key: EnvVariables): string | boolean;
+    set(key: EnvVariables, value: string): void;
+  }
+}
