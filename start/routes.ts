fd9bcbd45f6f3b75e411b1ae8c5c9ca569ecf5ec
Rishabh Jain <rishabh@codeation.io>
2020-08-01 17:58:47 +0530

    chore: added spotify auth login

--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,17 @@
 |
 */
 
+import querystring from 'querystring'
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
+Route.get('/login/spotify', async (ctx) => {
+  ctx.response.redirect(
+    'https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: 'user-read-private user-read-email',
+        redirect_uri: 'http://localhost:3000/shopify/callback',
+      })
+  )
 })
