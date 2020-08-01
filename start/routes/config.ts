95ca03467979a3f1f2e7b8bbe5f88ec1c84342c5
Rishabh Jain <rishabh@codeation.io>
2020-08-02 00:03:25 +0530

    feat: oauth config response

--- start/routes/config.ts
+++ start/routes/config.ts
@@ -1,18 +1,23 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/config/global', async (ctx) => {
-  ctx.response.json({
-    netflix: {
-      authorizationType: 'loginCookies',
-      login: 'https://www.netflix.com/in/login',
-      fetchCSV:
-        'https://www.netflix.com/api/shakti/ve0d6d2cb/viewingactivitycsv',
-    },
-    spotify: {
-      authorizationType: 'serverOAuthQuery',
-      oAuthUrl: 'google.com',
-      successQueryParameter: 'code',
-      failureQueryParameter: '',
-    },
+Route.group(() => {
+  Route.get('/config/global', async ({ response }) => {
+    response.json({
+      status: 'ok',
+      data: {
+        netflix: {
+          authorizationType: 'loginCookies',
+          login: 'https://www.netflix.com/in/login',
+          fetchCSV:
+          'https://www.netflix.com/api/shakti/ve0d6d2cb/viewingactivitycsv',
+        },
+        spotify: {
+          authorizationType: 'serverOAuthQuery',
+          oAuthUrl: 'google.com',
+          successQueryParameter: 'code',
+          failureQueryParameter: '',
+        },
+      },
+    })
   })
-})
+}).prefix('/api/v1')
