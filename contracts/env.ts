0bcb4433f5509c9b860222cbaf80de2539682123
Rishabh Jain <rishabh@codeation.io>
2020-08-02 00:39:25 +0530

    chore: added APP_FRONTEND env variable

--- contracts/env.ts
+++ contracts/env.ts
@@ -1,7 +1,7 @@
 declare module '@ioc:Adonis/Core/Env' {
   type EnvVariables = 'PORT' | 'HOST' | 'NODE_ENV' | 'APP_KEY' |
   'SPOTIFY_CLIENT_ID' | 'SPOTIFY_CLIENT_SECRET' | 'DB_CONNECTION' |
-  'DB_HOST' | 'DB_USER' | 'DB_PASSWORD' | 'DB_NAME'
+  'DB_HOST' | 'DB_USER' | 'DB_PASSWORD' | 'DB_NAME' | 'APP_FRONTEND'
 
   export interface EnvContract {
     process(envString: string, overwrite?: boolean): void;
