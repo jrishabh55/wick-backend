a863ebd6ade4008c3b411a8fc0171b9ddf704896
Rishabh Jain <rishabh@codeation.io>
2020-08-07 19:11:30 +0530

    feat: spotify  config from API

--- start/spotify.ts
+++ start/spotify.ts
@@ -1,9 +1,10 @@
 import SpotifyWebApi from 'spotify-web-api-node'
+import Env from '@ioc:Adonis/Core/Env'
 
 export const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'http://www.example.com/callback',
+  clientId: Env.get('SPOTIFY_CLIENT_ID') as string,
+  clientSecret: Env.get('SPOTIFY_CLIENT_SECRET') as string,
+  redirectUri: Env.get('APP_FRONTEND') as string,
 })
 
 export default spotifyApi
