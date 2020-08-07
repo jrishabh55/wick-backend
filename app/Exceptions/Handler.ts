94d0a3bcf46b4320511ff4414f3ec6ff73be4e8d
Rishabh Jain <rishabh@codeation.io>
2020-08-08 02:26:53 +0530

    feat: chaged base exception

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,37 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor () {
     super(Logger)
+  }
+
+  public async handle (error: any, ctx: HttpContextContract) {
+    console.log('handling exception')
+    switch (ctx.request.accepts(['json'])) {
+      case 'json':
+        return this.handleErrorWithApi(error, ctx)
+    }
+
+    return super.handle(error, ctx)
+  }
+
+  private async handleErrorWithApi (error: any, ctx: HttpContextContract) {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(error)
+      ctx.response.status(error.status || 500).json({
+        status: 'error',
+        error : {
+          message: error.message,
+          stack: error.stack,
+          code: error.code,
+        },
+      })
+      return
+    }
+
+    ctx.response.status(error.status).json({ status: 'error', error: { message: error.message, code: error.code } })
   }
 }
