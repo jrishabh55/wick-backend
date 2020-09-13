3122d4b59a73f8722e2873482922f603c840b67d
Rishabh Jain <rishabh@codeation.io>
2020-09-13 20:55:52 +0530

    feat: changed getCredentials

--- app/Models/Token.ts
+++ app/Models/Token.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column, BelongsTo, belongsTo } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
+import spotifyApi from 'start/spotify'
 
 export default class Token extends BaseModel {
   @column({ isPrimary: true })
@@ -39,8 +40,32 @@ export default class Token extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
-  public getCredentials () {
+  public async getCredentials () {
     const { accessToken, refreshToken } = this
+    if (this.type === 'spotify') {
+      const interval = this.updatedAt.diffNow('minutes').minutes
+
+      if (interval + this.expiresIn >= 0) {
+        return {
+          accessToken,
+          refreshToken,
+        }
+      }
+
+      spotifyApi.setCredentials({ accessToken: this.accessToken, refreshToken: this.refreshToken })
+      const tokenRes = (await spotifyApi.refreshAccessToken()).body
+      this.accessToken = tokenRes.access_token
+      this.expiresIn = tokenRes.expires_in
+      this.scope = tokenRes.scope
+      this.tokenType = tokenRes.token_type
+      await this.save()
+
+      return {
+        accessToken: tokenRes.access_token,
+        refreshToken: this.refreshToken,
+      }
+    }
+
     return {
       accessToken,
       refreshToken,
