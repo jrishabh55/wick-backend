acb76caf8e22b14a58be1a96368d5e711c8bcaf3
Rishabh Jain <rishabh@codeation.io>
2020-09-13 20:52:50 +0530

    feat: scope change

--- start/spotify.ts
+++ start/spotify.ts
@@ -2,7 +2,14 @@ import SpotifyWebApi from 'spotify-web-api-node'
 import Env from '@ioc:Adonis/Core/Env'
 
 export const scopes: string[] = [
-  'user-read-private', 'user-read-email',
+  'user-read-private',
+  'user-read-email',
+  'user-read-recently-played',
+  'user-top-read',
+  'user-follow-read',
+  'user-read-currently-playing',
+  'playlist-read-private',
+  'user-read-private',
 ]
 
 export const spotifyApi = new SpotifyWebApi({
